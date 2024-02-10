4d51af13530b284239dba37cd6b20e324c930f37
Taha Al Akab <alakabtaha@gmail.com>
2024-02-10 01:24:25 +0000

    ALlowing access-control-origin

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const port = 3000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 app.use('/api/v1/mail', mailRouter);
 
 app.listen(port, () => {
