b5ff9f281774b15f390bef38934587c4f619eccd
Taha Al Akab <alakabtaha@gmail.com>
2024-02-10 00:59:48 +0000

    adding standard get route

--- routes/mail.js
+++ routes/mail.js
@@ -6,6 +6,6 @@ const upload = multer();
 const mailRouter = express.Router();
 
 // Attach a middleware to process the form data
-mailRouter.route('/').post(upload.array(), mailController.sendMail)
+mailRouter.route('/').post(upload.array(), mailController.sendMail).get((req, res) => res.status(200).json({ message: 'Welcome to the mail API'}));
 
 module.exports = mailRouter;
